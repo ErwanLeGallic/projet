3e11cf8d2aa1e2027b8833e8bea55ec3c11a3d30
ErwanLeGallic <erwan.le-gallic@isen.yncrea.fr>
2024-05-02 11:08:07 +0200

    clean project

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/sofia/Programmation/projet
-# Build directory: C:/Users/sofia/Programmation/projet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
